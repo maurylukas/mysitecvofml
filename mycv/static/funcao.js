6cbadc34e6458cf9e5b99eef9f66a40a02daf753
maurylukas <maurylukas@yahoo.com>
2022-01-26 15:33:19 -0300

    Fixed navbar
    Vertical navbar animation finally fixed.

--- mycv/static/funcao.js
+++ mycv/static/funcao.js
@@ -4,30 +4,13 @@ $(document).ready(function() {
     theme: "minimal"
   });
   $('#sidebarCollapse').on('click', function() {
-      if ($('.wrapper').hasClass('active')) {
-        primeiroid ();
-      }
-      else //testar a ideia de criar um novo div abaixo do wrapper com um id pra ficar ativo no lugar
-      {
-        primeiroid ();
-      }
+    $('#sidebar, #content').toggleClass('active');
   });
-  function primeiroid ()
-  {
-    $('#sidebar, #content').toggleClass('active');
-    depoisclasse ();
-  }
-  function depoisclasse ()
-  {
-    $('.wrapper').toggleClass('active');
-  }
-  function primeiroclasse ()
-  {
-      $('.wrapper').toggleClass('active');
-      depoisid ();
-  }
-  function depoisid ()
-  {
-    $('#sidebar, #content').toggleClass('active');
-  }
+  $(window).on('scroll', function () {
+        if ($(window).scrollTop() >= 10) {
+            $('.wrapper').removeClass('active');
+        } else {
+            $('.wrapper').addClass('active');
+        }
+  });
 });
