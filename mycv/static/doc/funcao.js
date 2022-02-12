0f64c36366794ae6b3f76a14f5da2b6b16f105f9
maurylukas <maurylukas@yahoo.com>
2022-02-12 18:56:10 -0300

    Images changed
    Deleted some unused images, added some other images and made function to randomize them by reloading page.

--- mycv/static/doc/funcao.js
+++ mycv/static/doc/funcao.js
@@ -6,14 +6,27 @@ $(document).ready(function() {
   $('#sidebarCollapse').on('click', function() {
     $('#sidebar, #content').toggleClass('active');
   });
+
+  // Sidebar fix behavior
   $(window).on('scroll', function () {
-        if ($(window).scrollTop() >= 10) {
-            $('.wrapper').removeClass('active');
-        } else {
-            $('.wrapper').addClass('active');
-        }
+    if ($(window).scrollTop() >= 10) {
+        $('.wrapper').removeClass('active');
+    } else {
+        $('.wrapper').addClass('active');
+    }
   });
+
+  // Collapse panel behavior
   if ($(window).width() <= 768) {
     $('.collapse').collapse();
+    }
+
+    // Randomizing photos
+    if (Math.random () < 1 / 2) {
+        $("#me").attr ("src", "static/img/mim.jpg");
+        $("#us").attr ("src", "static/img/gente.jpg");
+    } else {
+        $("#me").attr ("src", "static/img/eu.jpg");
+        $("#us").attr ("src", "static/img/nos.jpg");
     }
 });
