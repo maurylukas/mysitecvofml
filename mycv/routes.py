d6dab2856b78d134739b8586bee594161ea15c29
maurylukas <97759783+maurylukas@users.noreply.github.com>
2022-03-20 18:24:33 -0300

    App added
    Added app file, updated link and made several adjustments to behaviour correctly on mobiles

--- mycv/routes.py
+++ mycv/routes.py
@@ -7,18 +7,3 @@ from flask import render_template, url_for
 @app.route ('/')
 def home ():
     return render_template ('home.html')
-
-# resume path
-@app.route ('/resume')
-def resume ():
-    return render_template ('resume.html')
-
-# projects path
-@app.route ('/projects')
-def projects ():
-    return render_template ('projects.html')
-
-# contact path
-@app.route ('/contact')
-def contact ():
-    return render_template ('contact.html')
--- mycv/static/doc/funcao.js
+++ mycv/static/doc/funcao.js
@@ -1,26 +1,4 @@
 $(document).ready(function() {
-  // Sidebar toggle behavior
-  $("#sidebar").mCustomScrollbar({
-    theme: "minimal"
-  });
-  $('#sidebarCollapse').on('click', function() {
-    $('#sidebar, #content').toggleClass('active');
-  });
-
-  // Sidebar fix behavior
-  $(window).on('scroll', function () {
-    if ($(window).scrollTop() >= 10) {
-        $('.wrapper').removeClass('active');
-    } else {
-        $('.wrapper').addClass('active');
-    }
-  });
-
-  // Collapse panel behavior
-  if ($(window).width() <= 768) {
-    $('.collapse').collapse();
-    }
-
     // Randomizing photos
     if (Math.random () < 1 / 2) {
         $("#me").attr ("src", "static/img/mim.jpg");
@@ -28,5 +6,35 @@ $(document).ready(function() {
     } else {
         $("#me").attr ("src", "static/img/eu.jpg");
         $("#us").attr ("src", "static/img/nos.jpg");
+    }
+
+    // Sidebar fix behavior
+    function scrolling () {
+        if ($(window).scrollTop() >= 10) {
+            $('.wrapper').removeClass('active');
+        } else {
+            $('.wrapper').addClass('active');
+        }
+    }
+    scrolling ();
+  $(window).on('scroll', scrolling);
+  $(document.body).on('touchmove', scrolling);
+
+  // Sidebar toggle behavior
+  $("#sidebar").mCustomScrollbar({
+    theme: "minimal"
+  });
+  $('#sidebarCollapse').on('click', function() {
+    $('#sidebar, #content').toggleClass('active');
+  });
+  $(".verify").on("click", function () {
+    if ($(window).width() <= 768) {
+        $('#sidebar, #content').toggleClass('active');
+    }
+  });
+
+    // Collapse panel behavior
+    if ($(window).width() <= 768) {
+        $('.collapse').collapse();
     }
 });
